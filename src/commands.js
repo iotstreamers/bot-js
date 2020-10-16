cfe843538e96298d1174dfcb8212a20f3d2eaf70
RodrigoDornelles <rodrigo@dornelles.me>
2020-10-15 22:55:33 -0300

    improve docs & new commands !source !github

--- src/commands.js
+++ src/commands.js
@@ -14,11 +14,26 @@ const command = new EventBus();
  *  | (_| (_) | |\/| | |\/| |/ _ \| .` | |) \__ \
  *   \___\___/|_|  |_|_|  |_/_/ \_\_|\_|___/|___/
  * 
- * @description adicione aqui os seus comandos!                                          
+ * @description adicione aqui os seus comandos!  
+ *   
+ * @argument params (Array de Strings) contendo parametros após o comando
+ * @argument message (Objeto)
+ * 
+ * @example message.send("sua mensagem") é utilizado para responder o comando
+ * @example message.is_discord ou message.is_twitch pode ser utilizado para identificar local
+ *
  */
 
 command.on("discord", (params, message) => {
-    message.send("Discord dos IoT Streamers: https://discord.gg/Gk5e5Cx");
+    message.send("Discord IoT Streamers: https://discord.gg/Gk5e5Cx");
+});
+
+command.on("github", (params, message) => {
+    message.send("Github IoT Streamers: https://github.com/iotstreamers");
+});
+
+command.on("source", (params, message) => {
+    message.send("Confira o meu código fonte: https://github.com/iotstreamers/discord-bot");
 });
 
 module.exports = command;
