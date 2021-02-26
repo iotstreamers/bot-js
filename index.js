ce5eaecccae1cf6efdc99c6b4a7b505c06ee0bfc
RodrigoDornelles <rodrigo@dornelles.me>
2021-02-26 11:38:09 -0300

    improves async & env

--- index.js
+++ index.js
@@ -1,79 +1,97 @@
 /**
- * @description Importaçao de blibiotecas
+ * @description Importaçao de blibiotecas e preparação de ambiente
  * 
  * @requires discord.js
  * @requires tmi.js
- * @requires fs (filesystem)
  * 
  */
+require('dotenv').config()
+const channels = process.env.TWITCH_CHANNELS.split(',');
 const DiscordConnect = require('discord.js');
 const commands = require('./src/commands.js');
 const modcmds = require('./src/moderation.js');
 const tmi = require('tmi.js');
-const fs = require('fs');
+const discord = {};
+const twitch = {};
 
 /**
- * @description Configurar Opções
+ * @description Carregamento assicrono das API's
  */
-const configuration = JSON.parse(fs.readFileSync('config.json'));
-const twitch = new tmi.client(configuration.twitch_options);
-const discord = new DiscordConnect.Client();
+discord.promise = new Promise(async (resolve) => {
+    discord.client = new DiscordConnect.Client();
+    resolve(console.log('[!] loading discord client...'));
+});
+twitch.promise = new Promise(async (resolve) => {
+    twitch.client = new tmi.client({
+        "twitch_options": {
+            "identity": {
+                "username": process.env.TWITCH_BOT_USERNAME,
+                "password": process.env.TWITCH_OAUTH_TOKEN
+            },
+            "channels": channels.map((channel) => '#' + channel)
+        }
+    });
+    resolve(console.log('[!] loading twitch options...'));
+});
+
 
 /**
  * @description Conexão com as APIS
  */
-twitch.connect();
-
-twitch.on("connected", () => {
-    console.log(`Twitch Ready!`);
-    discord.login(configuration.bot_discord_token);   
+discord.promise.then(async () => {
+    await discord.client.login(process.env.DISCORD_SECRET_TOKEN);
+    console.log(`[!] discord connected!`);
+});
+twitch.promise.then(async () => {
+    await twitch.client.connect();
+    console.log(`[!] twitch connected!`);
 });
 
-discord.on('ready', () => {
-    console.log(`Discord Ready!`);
-});
 
 /**
  * @description Quando alguem envia menssagem pela twitch
  * @see raw_message tem os dados brutos de qualquer evento de chat da live (messangem, highlight, host, raid)
  */
-twitch.on("raw_message", (messageCloned, message) => {
-    // ignorar quando não for uma menssagem no chat ou comando
-    if (message.command != "PRIVMSG" || message.params[1][0] != "!") {
-        return;
-    }
+twitch.promise.then(async () => {
+    console.log(`[!] creating twitch commands...`);
+    twitch.client.on("raw_message", (messageCloned, message) => {
+        // ignorar quando não for uma menssagem no chat ou comando
+        if (message.command != "PRIVMSG" || message.params[1][0] != process.env.TWITCH_COMMAND_PREFIX) {
+            return;
+        }
 
-    // preparar para executar comand
-    let is_moderator = typeof(message.tags["badges"]) == 'string' && (message.tags["badges"].indexOf("moderator") != -1 || message.tags["badges"].indexOf("broadcaster") != -1);
-    let channel = message.params[0];
-    let params = message.params[1].slice(1).split(' ');
-    let command = params.shift().toLowerCase(); 
-    let author = message.tags['display-name'];
+        // preparar para executar comand
+        let is_moderator = typeof(message.tags["badges"]) == 'string' && (message.tags["badges"].indexOf("moderator") != -1 || message.tags["badges"].indexOf("broadcaster") != -1);
+        let channel = message.params[0];
+        let params = message.params[1].slice(1).split(' ');
+        let command = params.shift().toLowerCase(); 
+        let author = message.tags['display-name'];
 
-    // não possui permissão
-    if (modcmds.handlersCount(command) && !is_moderator) {
-        twitch.say(channel, `@${author}, Você não é um moderador!`);
-        return;
-    } 
-    
-    // executar comando
-    commands.emit(command, params, {
-        is_twitch: true,
-        is_discord: false,
-        is_moderator: is_moderator,
-        reply: (text) => twitch.say(channel, `@${author}, ${text}`),
-        send: (text) => twitch.say(channel, text),
-        channels: configuration.twitch_options.channels.map((channel) => channel.slice(1))
-    });
-    
-    // executar comando administrativo
-    modcmds.emit(command, params, {
-        is_twitch: true,
-        is_discord: false,
-        is_moderator: is_moderator,
-        reply: (text) => twitch.say(channel, `@${author}, ${text}`),
-        send: (text) => twitch.say(channel, text),
-        channels: configuration.twitch_options.channels.map((channel) => channel.slice(1))
+        // não possui permissão
+        if (modcmds.handlersCount(command) && !is_moderator) {
+            twitch.client.say(channel, `@${author}, Você não é um moderador!`);
+            return;
+        } 
+        
+        // executar comando
+        commands.emit(command, params, {
+            is_twitch: true,
+            is_discord: false,
+            is_moderator: is_moderator,
+            reply: (text) => twitch.client.say(channel, `@${author}, ${text}`),
+            send: (text) => twitch.client.say(channel, text),
+            channels: channels
+        });
+        
+        // executar comando administrativo
+        modcmds.emit(command, params, {
+            is_twitch: true,
+            is_discord: false,
+            is_moderator: is_moderator,
+            reply: (text) => twitch.client.say(channel, `@${author}, ${text}`),
+            send: (text) => twitch.client.say(channel, text),
+            channels: channels
+        });
     });
 });
 
@@ -81,40 +99,43 @@ twitch.on("raw_message", (messageCloned, message) => {
  * @description Quando alguem envia menssagem pelo discord
  * @see message retorna objeto com api do discord para responder a menssagem
  */
-discord.on('message', (message) => {
-    // ignorar quando não for um comando
-    if (message.content[0] != "!") {
-        return;
-    }
+discord.promise.then(async () => {
+    console.log(`[!] creating discord commands...`);
+    discord.client.on('message', (message) => {
+        // ignorar quando não for um comando
+        if (message.content[0] != process.env.DISCORD_COMMAND_PREFIX) {
+            return;
+        }
 
-    // preparar para executar comandos
-    let is_moderator = message.member.roles.cache.has("763399481332334683");
-    let params = message.content.slice(1).split(' ');
-    let command = params.shift().toLowerCase(); 
+        // preparar para executar comandos
+        let is_moderator = message.member.roles.cache.has("763399481332334683");
+        let params = message.content.slice(1).split(' ');
+        let command = params.shift().toLowerCase(); 
 
-    // não possui permissão
-    if (modcmds.handlersCount(command) && !is_moderator) {
-        message.reply("Você não é um moderador!");
-        return;
-    } 
+        // não possui permissão
+        if (modcmds.handlersCount(command) && !is_moderator) {
+            message.reply("Você não é um moderador!");
+            return;
+        } 
 
-    // executar comando
-    commands.emit(command, params, {
-        is_twitch: false,
-        is_discord: true,
-        is_moderator: is_moderator,
-        reply: (text) => message.reply(text),
-        send: (text) => message.channel.send(text),
-        channels: configuration.twitch_options.channels.map((channel) => channel.slice(1))
-    });
+        // executar comando
+        commands.emit(command, params, {
+            is_twitch: false,
+            is_discord: true,
+            is_moderator: is_moderator,
+            reply: (text) => message.reply(text),
+            send: (text) => message.channel.send(text),
+            channels: channels
+        });
 
-    // executar comando administrativo
-    modcmds.emit(command, params, {
-        is_twitch: false,
-        is_discord: true,
-        is_moderator: is_moderator,
-        reply: (text) => message.reply(text),
-        send: (text) => message.channel.send(text),
-        channels: configuration.twitch_options.channels.map((channel) => channel.slice(1))
+        // executar comando administrativo
+        modcmds.emit(command, params, {
+            is_twitch: false,
+            is_discord: true,
+            is_moderator: is_moderator,
+            reply: (text) => message.reply(text),
+            send: (text) => message.channel.send(text),
+            channels: channels
+        });
     });
 });
